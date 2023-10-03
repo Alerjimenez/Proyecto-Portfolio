6cda7e3e1b4e4c9aebd755fb27af2ccf9e558243
Alejandro Rosales <alerjimenez@gmail.com>
2023-10-03 02:42:18 -0400

    se creo para que cambie el background

--- script.js
+++ script.js
@@ -10,5 +10,6 @@ const swith = document.querySelector(".switch");
 
     swith.addEventListener("click" , e => {
         swith.classList.toggle("active");
+        document.body.classList.toggle("active");
 
 })
