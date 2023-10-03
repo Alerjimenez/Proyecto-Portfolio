fb1392b2b5d4acca10baf8019d0ac7ff0a5c4d28
Alejandro Rosales <alerjimenez@gmail.com>
2023-10-03 02:29:04 -0400

    falta que cambie el background

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ function toggleMenu () {
     const icon = document.querySelector(".hamburger__icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+
 }
+
+const swith = document.querySelector(".switch");
+
+    swith.addEventListener("click" , e => {
+        swith.classList.toggle("active");
+
+})
